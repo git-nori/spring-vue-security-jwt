7c0965097df3f2bf25aa9c017dc63edca92c127c
HironoriSato <maza.hs5360@gmail.com>
2019-11-07 21:34:41 +0900

    [add]UserDetailsServiceの実装クラスを作成

---
+++ backend/src/main/java/com/example/demo/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.demo.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.User;
+import com.example.demo.repository.UserRepository;
+
+// カスタムUserDetailsService
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 認証に使用するユーザーをDBから取得し、返す
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user); // 該当ユーザーの認可情報を取得するためbuildメソッド(UserDetailsImplクラス)を呼び出す
+    }
+
+}
