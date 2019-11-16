1238e629a2f26cfefa4630851b635adcd8e94278
HironoriSato <maza.hs5360@gmail.com>
2019-11-16 10:34:44 +0900

    [add]Requestモデルを作成

---
+++ backend/src/main/java/com/example/demo/payload/request/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.example.demo.payload.request;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+}
