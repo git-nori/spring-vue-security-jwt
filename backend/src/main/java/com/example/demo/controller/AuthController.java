34e78c40ca86af5e11a04a9db9ff5813c9a919e1
HironoriSato <maza.hs5360@gmail.com>
2019-11-19 00:08:00 +0900

    [ref]messageを外部ファイルから取得するよう修正

--- backend/src/main/java/com/example/demo/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -20,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.config.SuccessContext;
+import com.example.demo.config.SyserrContext;
 import com.example.demo.models.ERole;
 import com.example.demo.models.Role;
 import com.example.demo.models.User;
@@ -51,6 +53,12 @@ public class AuthController {
     @Autowired
     JwtUtils jwtUtils;
 
+    @Autowired
+    private SyserrContext syserrContext;
+
+    @Autowired
+    private SuccessContext successContext;
+
     @PostMapping("/signin")
     public ResponseEntity authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
         Authentication authentication = authenticationManager.authenticate(
@@ -75,12 +83,12 @@ public class AuthController {
     public ResponseEntity registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
         if (userRepository.existsByUsername(signUpRequest.getUsername())) {
             // userNameがすでに存在する場合400エラーを返す
-            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already in t!"));
+            return ResponseEntity.badRequest().body(new MessageResponse(syserrContext.getExistsUserName()));
         }
 
         if (userRepository.existsByEmail(signUpRequest.getEmail())) {
             // emailがすでに存在する場合400エラーを返す
-            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
+            return ResponseEntity.badRequest().body(new MessageResponse(syserrContext.getExistsEmail()));
         }
 
         User user = new User(signUpRequest.getUsername(),
@@ -92,26 +100,26 @@ public class AuthController {
 
         if (strRoles == null) {
             Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                    .orElseThrow(() -> new RuntimeException("Error: Role is not found"));
+                    .orElseThrow(() -> new RuntimeException(syserrContext.getNotFoundRole()));
             roles.add(userRole);
         } else {
             strRoles.forEach(role -> {
                 switch (role) {
                 case "admin":
                     Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-                            .orElseThrow(() -> new RuntimeException("Error: Role is not found"));
+                            .orElseThrow(() -> new RuntimeException(syserrContext.getNotFoundRole()));
                     roles.add(adminRole);
 
                     break;
                 case "mod":
                     Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-                            .orElseThrow(() -> new RuntimeException("Error: Role is not found"));
+                            .orElseThrow(() -> new RuntimeException(syserrContext.getNotFoundRole()));
                     roles.add(modRole);
 
                     break;
                 default:
                     Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                            .orElseThrow(() -> new RuntimeException("Error: Role is not Found"));
+                            .orElseThrow(() -> new RuntimeException(syserrContext.getNotFoundRole()));
                     roles.add(userRole);
                 }
             });
@@ -120,6 +128,6 @@ public class AuthController {
         user.setRoles(roles);
         userRepository.save(user);
 
-        return ResponseEntity.ok(new MessageResponse("Uesr registered successfully!"));
+        return ResponseEntity.ok(new MessageResponse(successContext.getRegistered()));
     }
 }
