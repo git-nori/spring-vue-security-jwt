c8c21b5e6ae473ad81b6a270fd05852b6aba75e7
HironoriSato <maza.hs5360@gmail.com>
2019-11-19 11:08:52 +0900

    [add]vuexで使用する認証モジュールを作成

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { auth } from './auth.module'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    auth // 外部で定義した認証のためのvuexモジュールを設定
   }
 })
