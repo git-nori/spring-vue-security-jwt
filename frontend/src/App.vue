3712d687257f427c7877779adc5d8d179993e479
HironoriSato <maza.hs5360@gmail.com>
2019-11-19 14:14:56 +0900

    [add]navigationを追加

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,92 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <div class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a href="/home" class="nav-link">
+              <font-awesome-icon icon="home" />Home
+            </a>
+          </li>
+          <li class="nav-item" v-if="showAdminBoard">
+            <a href="/admin" class="nav-link">Admin Board</a>
+          </li>
+          <li class="nav-item" v-if="showModeratorBoard">
+            <a href="/mod" class="nav-link">Moderator Board</a>
+          </li>
+          <li class="nav-item">
+            <a href="/user" class="nav-link" v-if="currentUser">User</a>
+          </li>
+        </div>
+
+        <div class="navbar-nav ml-auto" v-if="!currentUser">
+          <li class="nav-item">
+            <a href="/register" class="nav-link">
+              <font-awesome-icon icon="user-plus" />Sign Up
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/login" class="nav-link">
+              <font-awesome-icon icon="sign-in-alt" />Login
+            </a>
+          </li>
+        </div>
+
+        <div class="navbar-nav ml-auto" v-if="currentUser">
+          <li class="nav-item">
+            <a href="/profile" class="nav-link">
+              <font-awesome-icon icon="user" />
+              {{currentUser.username}}
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href class="nav-link" @click="logOut">
+              <font-awesome-icon icon="sign-out-alt" />LogOut
+            </a>
+          </li>
+        </div>
+      </nav>
     </div>
-    <router-view/>
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  computed: {
+    currentUser() {
+      console.log(this.$store.state.auth.user);
+      return this.$store.state.auth.user;
+    },
+    showAdminBoard() {
+      if (this.currentUser) {
+        return this.currentUser.roles.includes("ROLE_ADMIN");
+      }
+
+      return false;
+    },
+    showModeratorBoard() {
+      if (this.currentUser) {
+        return this.currentUser.roles.includes("ROLE_MODERATOR");
+      }
+
+      return false;
+    }
+  },
+  methods: {
+    logOut() {
+      this.$store.dispatch("auth/logout");
+      this.$router.push("/login");
+    }
+  }
+};
+</script>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
