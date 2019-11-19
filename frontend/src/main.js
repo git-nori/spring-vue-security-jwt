776b349c7a83e3bc9b2d8963b46116bef1d3b590
HironoriSato <maza.hs5360@gmail.com>
2019-11-19 12:08:54 +0900

    [add]vuetifyを使用するよう設定

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VeeValidate from 'vee-validate'
+import vuetify from 'vuetify'
 
 Vue.config.productionTip = false
 
 Vue.use(VeeValidate)
+Vue.use(vuetify)
 
 new Vue({
   router,
